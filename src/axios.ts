dd87d3bd7ad20e1baaf5b2f811f25e8e7cb6358a
Arash Salehkhah <southernwavesofmadness@protonmail.com>
2023-10-04 12:24:36 +0330

    Fix API Route

--- src/axios.ts
+++ src/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = import.meta.env.VITE_LOCAL_API || 'https://abyss-api.chill-hub.net';
+const api = import.meta.env.VITE_API || 'https://abyssapi.chill-hub.net';
 axios.defaults.baseURL = api;
 export default axios;
